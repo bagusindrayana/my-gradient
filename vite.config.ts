d350529f6049ec4ffda8198de3266594c430542c
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-05-12 08:28:19 +0800

    remov wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import type { UserConfig } from 'vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),cloudflare()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
