593559252dfb73cda38085007fbc63c06c206be4
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-05-12 08:21:37 +0800

    test cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import type { UserConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),cloudflare()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
